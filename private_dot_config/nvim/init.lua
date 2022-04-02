05eeb8dfc03db8ba188233ece205baf6c868ef43
Omar Abdeldayem <omar.abdeldayem@outlook.com>
2022-04-02 14:21:01 -0700

    Update nvim config w/ NvChad

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,67 +1,31 @@
-require('lv-globals')
-vim.cmd('luafile '..CONFIG_PATH..'/lv-settings.lua')
-require('settings')
-require('lv-gitblame')
-require('plugins')
-require('lv-utils')
-require('lv-autocommands')
-require('keymappings')
-require('lv-nvimtree') -- This plugin must be required somewhere before colorscheme.  Placing it after will break navigation keymappings
-require('colorscheme') -- This plugin must be required somewhere after nvimtree. Placing it before will break navigation keymappings
-require('lv-galaxyline')
-require('lv-comment')
-require('lv-gitblame')
-require('lv-compe')
-require('lv-barbar')
-require('lv-dashboard')
-require('lv-telescope')
-require('lv-gitsigns')
-require('lv-treesitter')
-require('lv-matchup')
-require('lv-autopairs')
-require('lv-rnvimr')
-require('lv-which-key')
-require('lv-lsp-rooter')
-require('lv-zen')
+local present, impatient = pcall(require, "impatient")
 
--- extras
-if O.extras then
-    require('lv-numb')
-    require('lv-hop')
-    require('lv-colorizer')
+if present then
+   impatient.enable_profile()
 end
 
+local core_modules = {
+   "core.options",
+   "core.autocmds",
+   "core.mappings",
+}
 
+for _, module in ipairs(core_modules) do
+   local ok, err = pcall(require, module)
+   if not ok then
+      error("Error loading " .. module .. "\n\n" .. err)
+   end
+end
 
--- TODO is there a way to do this without vimscript
-vim.cmd('source '..CONFIG_PATH..'/vimscript/functions.vim')
+-- non plugin mappings
+require("core.mappings").misc()
 
--- LSP
-require('lsp')
--- require('lsp.bash-ls')
-require('lsp.clangd')
--- require('lsp.css-ls')
--- require('lsp.dart-ls')
--- require('lsp.docker-ls')
--- require('lsp.efm-general-ls')
--- require('lsp.elm-ls')
--- require('lsp.emmet-ls')
--- require('lsp.graphql-ls')
--- require('lsp.go-ls')
--- require('lsp.html-ls')
--- require('lsp.json-ls')
--- require('lsp.js-ts-ls')
--- require('lsp.kotlin-ls')
--- require('lsp.latex-ls')
---require('lsp.lua-ls')
---require('lsp.php-ls')
-require('lsp.python-ls')
---require('lsp.ruby-ls')
-require('lsp.rust-ls')
---require('lsp.svelte-ls')
---require('lsp.terraform-ls')
--- require('lsp.tailwindcss-ls')
---require('lsp.vim-ls')
---require('lsp.vue-ls')
-require('lsp.yaml-ls')
---require('lsp.elixir-ls')
+-- check if custom init.lua file exists
+if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/custom/init.lua") == 1 then
+   -- try to call custom init, if not successful, show error
+   local ok, err = pcall(require, "custom")
+   if not ok then
+      vim.notify("Error loading custom/init.lua\n\n" .. err)
+   end
+   return
+end
