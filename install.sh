97d3a313d70bdfe871fd36b196717dd2b49f757e
Omar Abdeldayem <omar.abdeldayem@outlook.com>
2022-12-11 20:47:54 -0600

    Abandon chezmoi altogether

--- install.sh
+++ install.sh
@@ -15,10 +15,21 @@ else
   ${SUDO_PREFIX} yum -y install ${APPS[@]}
 fi
 
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
+
 # Install NVIM
 git clone https://github.com/neovim/neovim.git
 cd neovim
-git checkout v0.7.2
+git checkout v0.8.0
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 ${SUDO_PREFIX} make install
 
+# TODO: Don't clobber by default
+cp -r nvim ~/.config/nvim
+cp -r i3 ~/.config/i3
+cp -r polybar ~/.config/polybar
+
+cp dot_zshrc ~/.zshrc
+cp -r dot_oh-my-zsh ~/.oh-my-zsh
+cp dot_tmux_conf ~/.tmux.conf
